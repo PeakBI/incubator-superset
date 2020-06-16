9e49a3d832d07525eab22b2a155be85d459c6c72
azhar22k <azhar.khan.78601@gmail.com>
2020-06-16 16:47:12 +0530

    ADDED logs to debug

--- superset/custom_auth.py
+++ superset/custom_auth.py
@@ -35,6 +35,8 @@ def use_ip_auth(f):
     Will return 404 for unknown IPs
     """
     def wraps(self, *args, **kwargs):
+        print('REQUEST RECIEVED')
+        print(request)
         client_ip = request.headers['x-forwarded-for'] if request.headers.get('x-forwarded-for') else request.remote_addr
         try:
             loads(call(
@@ -43,8 +45,10 @@ def use_ip_auth(f):
                 'ipAuth', {
                     'clientIp': client_ip
                 }))
+            print('IP AUTH SCCESS')
             return f(self, *args, **kwargs)
-        except Exception:
+        except Exception as e:
+            print('IP ERROR {}'.format(e))
             response = make_response(
                 jsonify({'message': 'Not Found', }),
                 404
@@ -61,18 +65,22 @@ class CustomAuthDBView(AuthDBView):
     def login(self):
         redirect_url = self.appbuilder.get_url_for_index
         user = 'guest'
+        print('REQUEST RECIEVED')
+        print(request)
         try:
             if request.args.get('redirect') is not None:
                 redirect_url = request.args.get('redirect')
 
             if request.args.get('authToken') is not None:
                 token = 'Bearer {}'.format(request.args.get('authToken'))
+                print('GOT TOKEN {}'.format(token))
                 auth_response = loads(call(
                     'ais-{}'.format(environ['STAGE']),
                     'authentication',
                     'auth', {
                         'authorizationToken': token
                     }))['context']
+                print('AUTH RESPONSE {}'.format(auth_response))
                 if not auth_response['tenant'] == environ['TENANT']:
                     raise Exception('Tenant mismatch in token')
                 if auth_response['role'] in ['tenantManager', 'tenantAdmin']:
@@ -83,17 +91,22 @@ class CustomAuthDBView(AuthDBView):
                         user = 'peakuser'
                     elif not has_resource_access(privileges):
                         raise Exception('Insufficient Resource Permissions')
+                print("LET US GET USER")
                 user = self.appbuilder.sm.find_user(user)
+                print('GOT USER', user)
                 login_user(user, remember=False,
                            duration=timedelta(
                             auth_response['exp'] - int(
                                 datetime.now().timestamp())))
+                print('LOGIN SUCCESSFUL')
                 return redirect(redirect_url)
             elif g.user is not None and g.user.is_authenticated:
                 return redirect(redirect_url)
             else:
                 raise Exception('Login is valid only through "authToken"')
         except Exception as e:
+            print('FAATA')
+            print(e)
             flash(e, 'warning')
             return super(CustomAuthDBView, self).login()
 
